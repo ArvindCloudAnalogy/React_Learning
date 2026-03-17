ea6f9b36a0ccea992875989e1f7799edf8aa5298
Arvind Gautam <arvind.gautam@cloudanalogy.com>
2026-03-17 16:22:32 +0530

    warning removed

--- Ui_Project/src/components/Section1/RightCardContent.jsx
+++ Ui_Project/src/components/Section1/RightCardContent.jsx
@@ -10,7 +10,7 @@ const RightCardContent = (props) => {
     }
 
     return (
-        <div className='p-7 justify-between flex flex-col absolute top-0 left-0 h-full w-full bg-gradient-to-t from-black/80 via-black/40 to-transparent text-white'>
+        <div className='p-7 justify-between flex flex-col absolute top-0 left-0 h-full w-full bg-linear-to-t from-black/80 via-black/40 to-transparent text-white'>
             <h2 className='bg-white/20 text-white rounded-full h-12 w-12 flex justify-center items-center text-xl font-bold'>{props.id + 1}</h2>
             <div>
                 <p className='text-lg leading-relaxed mb-14'>{props.intro}</p>
